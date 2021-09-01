2459b3167af0c8a2b5af1e0293f34a46237f9cf9
Anthony Trombetta <anthonytrombetta@anthony-tpg.local>
2021-09-01 08:26:23 -0500

    clean up mono repo.

--- src/interfaces/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './item.interface';
-export * from './list.interface';
--- src/interfaces/list.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Item } from './item.interface';
-
-export interface List {
-  id: string;
-  name: string;
-  type?: string;
-  items: { [key: string]: Item };
-}
